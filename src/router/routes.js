8c7ce4f8cceaf1764af79e67defea39dfbcc0d9d
hidaka <n.hidaka@gemcook.com>
2018-11-01 21:41:29 +0900

    [update] ### 不要な記述削除

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,7 @@
 import { Top } from "../pages/Top";
-import { Edit } from "../pages/Edit";
 
 const routes = [
   { path: '/', name: "Top", component: Top },
-  { path: '/edit', name: "Edit", component: Edit }
 ];
 
 export default routes;
